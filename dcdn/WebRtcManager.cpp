3bd131ded36536eb3b4454ef57359465268a6b6b
Antonadal1477 <antonadal1477@gmail.com>
2025-08-19 10:43:29 +0800

    ✨ feat: use async api with report net info

--- dcdn/WebRtcManager.cpp
+++ dcdn/WebRtcManager.cpp
@@ -112,7 +112,7 @@ void WebRtcManager::report()
         info["protocol"] = webRtcConnProtocol;
         info["connMeta"] = mSdp;
         msg["infos"].push_back(info);
-        mMan->ApiPostWithToken(mClient, "/api/v1/report_net_info", msg, nullptr);
+        mMan->AsyncApiPostWithToken(nullptr, "/api/v1/report_net_info", msg, this, nullptr, nullptr);
     } catch (std::exception& excp) {
         logWarn << "webrtc report exception: " << excp.what();
     } catch (...) {
